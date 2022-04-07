57b0a828ebd4dd113a14cce7ecba2341def3eac8
super Nicolás <nicolas.guevara@cea.edu.co>
2022-04-07 08:20:55 -0500

    ahfu

---
+++ src/practica.js
@@ -0,0 +1,21 @@
+/*javaScript como todo lenguaje de programacion se basa en la utilizacion de palabra reservada*/
+// creacion de variables
+var nom = "Nicolas"; //no se recomienda usarla
+count nombre = "Fernando"
+let apellido = "Guevara"
+
+alert (nombre);
+*/
+
+let numero = 3;
+let numero = 2;
+let suma = numero;
+print (suma);
+
+/*Operadores aritmeticos en javaScript
++ adicion
+- resta
+* multiplicacion
+/ division
+% residuo
+
