ac2f7a8d14f5d3863dabf82ed0a682019c44e726
super Nicolás <nicolas.guevara@cea.edu.co>
2022-05-12 08:11:10 -0500

    asad

--- src/practica2.js
+++ src/practica2.js
@@ -1,13 +1,17 @@
-num1= parseInt(prompt("Escribe un numero: "));
-num2= parseInt(prompt("Porfavor escribe otro numero: "));
-operaciones = prompt("Escribe cual operacion deseas realizar entre los dos numero que acabaste de escribir: ");
+let num1= parseInt(prompt("Escribe un numero: "));
+let num2= parseInt(prompt("Porfavor escribe otro numero: "));
+let operaciones = prompt("Escribe cual operacion deseas realizar entre los dos numero que acabaste de escribir: ");
 let resultado = 0;
 if(operaciones == "suma"){
-    let resultado = num1 + num2;
+     resultado = num1 + num2;
 }
 if(operaciones == "restar"){
-    let resultado = num1 - num2;
+    resultado = num1 - num2;
+}
     if(operaciones == "multiplicar"){
-        let resultado = num1 * num2;
+        resultado = num1 * num2;
+    }
         if(operaciones == "dividir"){
-            let resultado = num1 / num2;     
+            resultado = num1 / num2;
+        }
+        alert("El resultado de tu operacion es: " + resultado);
