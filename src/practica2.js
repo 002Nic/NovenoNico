4820a7731fb4a609d74bf5e2eea90445833575d4
super Nicolás <nicolas.guevara@cea.edu.co>
2022-05-19 08:13:54 -0500

    hjas

--- src/practica2.js
+++ src/practica2.js
@@ -2,16 +2,19 @@ let num1= parseInt(prompt("Escribe un numero: "));
 let num2= parseInt(prompt("Porfavor escribe otro numero: "));
 let operaciones = prompt("Escribe cual operacion deseas realizar entre los dos numero que acabaste de escribir: ");
 let resultado = 0;
-if(operaciones == "suma"){
+if(operaciones == "suma" || operaciones == "+"){
      resultado = num1 + num2;
 }
-if(operaciones == "restar"){
+else
+if(operaciones == "restar" || operaciones == "-"){
     resultado = num1 - num2;
 }
-    if(operaciones == "multiplicar"){
+else
+    if(operaciones == "multiplicar" || operaciones == "*"){
         resultado = num1 * num2;
     }
-        if(operaciones == "dividir"){
+    else
+        if(operaciones == "dividir" || operaciones == "/"){
             resultado = num1 / num2;
         }
         alert("El resultado de tu operacion es: " + resultado);
