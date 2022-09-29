b49af62bac55d3dad170ca45ad866cac873f6606
super Nicolás <nicolas.guevara@cea.edu.co>
2022-09-29 08:54:57 -0500

    perro

---
+++ Examen/sistemas/index.js
@@ -0,0 +1,6 @@
+function suma (){
+    num1= parseInt(prompt("Escribe un numero: "));
+    num2= parseInt(prompt("Porfavor escribe otro numero: "));
+    suma= num1 + num2;
+    resultado=alert("El resultado es:" + suma);
+    }
