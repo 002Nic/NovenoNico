f488ec43f071e6fdf8ec35dc7cd2b34d91b77f77
super Nicolás <nicolas.guevara@cea.edu.co>
2022-11-03 08:50:13 -0500

    VAMOS

--- 1ra.Web/Pagina web1/index.js
+++ 1ra.Web/Pagina web1/index.js
@@ -4,8 +4,15 @@ window.onload = () => {
     formulario.onsubmit = (e) => {
         e.preventDefault();
         const pendiente = document.getElementById("pendiente");
-        const contenido = pendiente.value;
+        const contenidoPendiente = pendiente.value;
         pendiente.value = "";
-        console.log (contenido);
+        pendientes.push (contenidoPendiente);
+        pendiente.innerHTML = "";
+        const listado = document.getElementById("por-hacer");
+        listado.innerHTML = "";
+        for (let i = 0; i < pendientes.length; i++) {
+            const e = pendientes[i];
+            listado.innerHTML += "<li>" + e + "</li>"
+        }
     }
 }
